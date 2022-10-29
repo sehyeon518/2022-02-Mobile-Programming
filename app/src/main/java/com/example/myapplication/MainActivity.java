f8d0a70e38684b39ecc733b12a7a02c27419c869
sehyeon518 <lifethis21@kookmin.ac.kr>
2022-10-29 23:19:43 +0900

    Preference and Arraylist

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.SecondActivity.sharePref;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,29 +9,44 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    EditText text_ID, text_PW;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text_ID = (EditText) findViewById(R.id.text_ID); // 아이디
+        text_PW = (EditText) findViewById(R.id.text_PW); // 비밀번호
+
+
+        // 로그인 버튼을 눌렀을 때
         Button button_login = (Button) findViewById(R.id.btn_log_in);
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // if 문: 로그인 가능할 때
-                Toast.makeText(getApplicationContext(), "호그와트 서점에 오신 걸 환영합니다",
-                        Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
-                startActivity(intent);
+                if(text_ID.getText().toString() != null) {
+                    String input_ID = sharePref.getString("ID", null);
+                    String input_PW = sharePref.getString("PW", null);
+                    Toast.makeText(getApplicationContext(), "호그와트 서점에 오신 걸 환영합니다", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
+                    startActivity(intent);
+                }
                 // else 문: 로그인 에러 발생했을 때
-//                Toast.makeText(getApplicationContext(), text:"비밀번호가 일치하지 않아요",
-//                        Toast.LENGTH_SHORT.show());
+                else {
+                    Toast.makeText(getApplicationContext(), "아이디 혹은 비밀번호가 일치하지 않아요", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+        // 회원가입 화면으로 이동
         Button btn_sign_up = (Button) findViewById(R.id.btn_sign_up);
         btn_sign_up.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -38,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 비회원 상품보기로 이동
         Button btnNoneMember = (Button) findViewById(R.id.btn_none_member);
         btnNoneMember.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -45,5 +63,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,51 +1,57 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends Activity {
-    // 접속 정보와 태그명 지정
-    private static final String TAG = "Member Join";
+    static private String SHARE_NAME = "SHARE_PREF";
+    static SharedPreferences sharePref = null;
+    static final String SETTINGS_PLAYER_JSON = "settings_item_json";
+
     // 뷰 객체 생성
-    EditText edit_name, edit_number, edit_address, edit_ID, edit_PW;
+    EditText user_name, user_number, user_address, user_ID, user_PW;
     RadioButton radio_agree;
     // 변수 객체 생성
     String name, number, address, ID, PW;
-    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        edit_name = (EditText) findViewById(R.id.user_name); // 이름
-        edit_number = (EditText) findViewById(R.id.user_number); // 전화번호
-        edit_address = (EditText) findViewById(R.id.user_address); // 주소
-        edit_ID = (EditText) findViewById(R.id.user_ID); // 아이디
-        edit_PW = (EditText) findViewById(R.id.user_PW); // 비밀번호
+        user_name = (EditText) findViewById(R.id.user_name); // 이름
+        user_number = (EditText) findViewById(R.id.user_number); // 전화번호
+        user_address = (EditText) findViewById(R.id.user_address); // 주소
+        user_ID = (EditText) findViewById(R.id.user_ID); // 아이디
+        user_PW = (EditText) findViewById(R.id.user_PW); // 비밀번호
         radio_agree = (RadioButton) findViewById(R.id.btn_info_agree); // 이용 동의
 
+        sharePref = getSharedPreferences(SHARE_NAME, MODE_PRIVATE); // 이 앱에서만 사용 가능
+
+        // 회원가입 완료 버튼 눌렀을 때
         Button btn_return = (Button) findViewById(R.id.btn_return);
         btn_return.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                name = edit_name.getText().toString();
-                number = edit_number.getText().toString();
-                address = edit_address.getText().toString();
-                ID = edit_ID.getText().toString();
-                PW = edit_PW.getText().toString();
+                name = user_name.getText().toString();
+                number = user_number.getText().toString();
+                address = user_address.getText().toString();
+                ID = user_ID.getText().toString();
+                PW = user_PW.getText().toString();
 
                 if (!radio_agree.isChecked()) {
                     Toast.makeText(getApplicationContext(), "약관을 읽고 동의 해주세요.", Toast.LENGTH_SHORT).show();
@@ -53,36 +59,79 @@ public class SecondActivity extends Activity {
                 }
                 else if (name.equals("")) {
                     Toast.makeText(getApplicationContext(), "이름을 입력해주세요", Toast.LENGTH_SHORT).show();
-                    edit_name.requestFocus();
+                    user_name.requestFocus();
                 }
                 else if (address.equals("")) {
                     Toast.makeText(getApplicationContext(), "주소를 입력해주세요", Toast.LENGTH_SHORT).show();
-                    edit_address.requestFocus();
+                    user_address.requestFocus();
                 }
                 else if (number.equals("")) {
                     Toast.makeText(getApplicationContext(), "전화번호를 입력해주세요", Toast.LENGTH_SHORT).show();
-                    edit_number.requestFocus();
+                    user_number.requestFocus();
                 }
                 else if (ID.equals("")) {
                     Toast.makeText(getApplicationContext(), "ID를 입력해주세요", Toast.LENGTH_SHORT).show();
-                    edit_ID.requestFocus();
+                    user_ID.requestFocus();
                 }
                 else if (PW.equals("")) {
                     Toast.makeText(getApplicationContext(), "PW를 입력해주세요", Toast.LENGTH_SHORT).show();
-                    edit_PW.requestFocus();
+                    user_PW.requestFocus();
                 }
                 else {
                     // 모든 입력이 정상적일 때
-                    signCheck();
-                    Toast.makeText(getApplicationContext(), "호그와트 서점으로 이동합니다", Toast.LENGTH_SHORT).show();
+//                    SharedPreferences.Editor editor = sharePref.edit(); // Editor를 preferences에 씀
+                    // putString
+                    ArrayList<String> list = new ArrayList<>();
+                    list.add(user_name.getText().toString());
+                    list.add(user_address.getText().toString());
+                    list.add(user_number.getText().toString());
+                    list.add(user_ID.getText().toString());
+                    list.add(user_PW.getText().toString());
+                    setStringArrayPref(SETTINGS_PLAYER_JSON, list);
+//                    editor.putString("name", user_name.getText().toString());
+//                    editor.putString("address", user_address.getText().toString());
+//                    editor.putString("number", user_number.getText().toString());
+//                    editor.putString("ID", user_ID.getText().toString());
+//                    editor.putString("PW", user_PW.getText().toString());
+//                    android.util.Log.v("SignUp", editor.toString());
+//                    editor.commit();
+                    Toast.makeText(getApplicationContext(), "회원가입 완료되었습니다", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    android.util.Log.d("Sigu Up", getStringArrayPref(SETTINGS_PLAYER_JSON).toString());
+                    startActivity(intent);
                 }
             }
         });
     }
-    protected void signCheck() {
-        editor.putString(ID, PW);
-        editor.commit();
+
+    private void setStringArrayPref(String key, ArrayList<String> values) {
+        SharedPreferences prefs = getSharedPreferences(SHARE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        JSONArray a = new JSONArray();
+        for (int i = 0; i < values.size(); i++) {
+            a.put(values.get(i));
+        }
+        if (!values.isEmpty()) editor.putString(key, a.toString());
+        else editor.putString(key, null);
+        editor.apply();
+    }
+    private ArrayList<String> getStringArrayPref(String key) {
+        SharedPreferences prefs = getSharedPreferences(SHARE_NAME, MODE_PRIVATE);
+        String json = prefs.getString(key, null);
+        ArrayList<String> urls = new ArrayList<String>();
+        if (json != null) {
+            try{
+                JSONArray a = new JSONArray(json);
+                for (int i = 0; i < a.length(); i++) {
+                    String url = a.optString(i);
+                    urls.add(url);
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return urls;
     }
 
 }
